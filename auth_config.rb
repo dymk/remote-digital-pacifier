bbcf9e2a581c8f05943b6983741cbdb5db30f456
Dylan Knutson <tcdknutson@gmail.com>
2017-08-22 22:30:19 -0700

    add dummy auth_config

---
+++ auth_config.rb
@@ -0,0 +1,16 @@
+def api_key
+  "telegram bot API key"
+end
+
+def bot_name
+  "something here"
+end
+
+def bot_username
+  bot_name
+end
+
+def admin_telegram_channel
+  # whatever your telegram ID is
+  00000000
+end
